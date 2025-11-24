b6b4acbf595a22e59edc688254ad6866045f9ffa
Mahmudulhasansiyam11 <mahmudulsiyam766@gmail.com>
2025-11-24 18:49:55 +0600

    add update api using patch

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config(); 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,45 +11,61 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o2upin6.mongodb.net/?appName=Cluster0`;
 
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-
-app.get('/', (req, res) => {
-    res.send('workout pro server is running');
-})
-
+app.get("/", (req, res) => {
+  res.send("workout pro server is running");
+});
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const workOutsDB = client.db('workOutsDB');
-    const workOutCollection = workOutsDB.collection('workOuts');
-
+    const workOutsDB = client.db("workOutsDB");
+    const workOutCollection = workOutsDB.collection("workOuts");
 
     // add database related apis here
     // add database api
-    app.post('/workOuts', async (req, res) => {
-        const newWorkOut = req.body;
-        console.log('workOut info', newWorkOut);
-        const result = await workOutCollection.insertOne(newWorkOut);
-        res.send(result);
-    })
+    app.post("/workOuts", async (req, res) => {
+      const newWorkOut = req.body;
+      console.log("workOut info", newWorkOut);
+      const result = await workOutCollection.insertOne(newWorkOut);
+      res.send(result);
+    });
 
-    // delete database
+    // updata database api
+    app.patch("/workOuts/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedWorkOut = req.body;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: updatedWorkOut,
+      };
+      const result = await workOutCollection.updateOne(query, update);
+      res.send(result);
+    });
+
+    // delete database api
+    app.delete("/workOuts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await workOutCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -57,7 +73,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 app.listen(port, (req, res) => {
-    console.log(`workout pro server is running on port: ${port}`);
-})
+  console.log(`workout pro server is running on port: ${port}`);
+});
